e7567909fb7c3665f303fd117126d57c4d51fc37
Vittorio Vaselli <vittorio.vaselli@outlook.com>
2020-03-06 18:54:02 +0100

    add favicon and footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ItemsContainer from './components/ItemsContainer'
 import Player from './components/Player';
 import Header from './components/Header'
 import SearchBar from './components/SearchBar';
+import Footer from './components/Footer';
 
 
 class App extends Component {
@@ -15,10 +16,14 @@ class App extends Component {
     return (
       <Provider store={store}>
         <div className="App">
-          <Header/>
-          <SearchBar/>
-          <ItemsContainer/> 
-          <Player/>
+          <div>
+            <Header/>
+            <SearchBar/>
+            <ItemsContainer/> 
+
+          </div>
+          <Player/>    
+          <Footer/>
         </div>
       </Provider>
     );
---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+class Footer extends Component {
+    
+
+    render() {
+        
+
+        return (
+        <div className="App-footer">
+                Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+                    Freepik 
+                </a> 
+                from 
+                <a href="https://www.flaticon.com/" title="Flaticon">
+                    www.flaticon.com
+                    </a>
+        </div>
+        );
+    }
+}
+
+
+  
+export default Footer;
